30fe4fbf4d24e283492493227666c79969b73ae0
khalid7487 <khalid15-7487@diu.edu.bd>
2022-10-13 23:31:33 +0600

    setup for  log

--- App/index.js
+++ App/index.js
@@ -3,11 +3,31 @@ import connectionWithDb from "./mongo";
 import configure from "./controllers";
 import { handleErrors } from './middlewares/handleErrors';
 
+import winston from "winston";
+import expressWinston from "express-winston";
+import winstonFile from "winston-daily-rotate-file"
+import winstonMongo from "winston-mongodb";
+import {  ElasticsearchTransport} from "winston-elasticsearch";
 
 const port = 7005;
 const app = express();
 
 app.use(express.json());
+
+const processRequest = async(req, res, next) => {
+     let correlationId = req.headers['x-correlation-id'];
+
+     if(!correlationId){
+        correlationId =Date.now().toString();
+        req.headers['x-correlation-id'] = correlationId;
+     }
+
+     res.set('x-correlation-id', correlationId);
+
+     return next();
+}
+
+app.use(processRequest);
 
 connectionWithDb();
 
